3adf1809e8938fd0e04b7fa80901fd00bfa1ebce
heloabrantessb <heloisa.abrantesdev@gmail.com>
2024-03-21 18:26:57 -0300

    Inicio da construção do  listComments

--- javascript/getPoints.js
+++ javascript/getPoints.js
@@ -3,3 +3,13 @@ export async function getPoints(){
     const data = await response.json()
     return data
 }
+export async function getComments(){
+    const response = await fetch (`https://paranaguatour-api.onrender.com/comments/2`)
+    const data = await response.json()
+    return data
+}
+export async function getUsers(user_id){
+    const response = await fetch (`https://paranaguatour-api.onrender.com/users/${user_id}`)
+    const data = await response.json()
+    return data
+}
--- javascript/renderPoints.js
+++ javascript/renderPoints.js
@@ -1,18 +1,16 @@
-import { getPoints } from "./getPoints.js";
+import { getComments, getPoints, getUsers } from "./getPoints.js";
 
-const introducao = document.getElementById('introducao');
+const introducao = document.getElementById("introducao");
 
-const [points] = await getPoints()
-console.log(points);
+const [points] = await getPoints();
+const comments = await getComments();
 
 introducao.innerHTML = `
 <section id="content">
-<h1>${points.name}</h1>
-<h2>${points.location}</h2>
-
-
+  <h1>${points.name}</h1>
+  <h2>${points.location}</h2>
   <div class="carrouselcontainer">
-  <div id="carouselExampleIndicators" class="carousel slide">
+    <div id="carouselExampleIndicators" class="carousel slide">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -39,23 +37,73 @@ introducao.innerHTML = `
       </button>
     </div>
   </div>
-
 </section>
 <section id="text">
-<div id="cardstyle" class="card">
-  <div class="card-header">
-    <h2>Sobre</h2>
+  <div id="cardstyle" class="card">
+    <div class="card-header">
+      <h2>Sobre</h2>
+    </div>
+    <div class="card-body">
+      <p class="card-text">${points.description}</p>
+    </div>
   </div>
-  <div class="card-body">
- 
-    <p class="card-text">${points.description}</p>
-   
-</div>
-</div>
-
 </section>
-
 <div class="ratingMedia">
-<div class="Stars" id="mediaStars"  style="--rating: ${points.mediaRating};" aria-label="">
+  <p class="numeroRating">${points.mediaRating}</p>
+  <div class="Stars" id="mediaStars"  style="--rating: ${points.mediaRating};" aria-label=""></div>
 </div>
-`
+`;
+
+const comentcard = document.getElementById('comentcard')
+
+async function renderComments() {
+
+  function commentsList(comments) {
+    const commentCard = document.createElement('div');
+    commentCard.classList.add('card');
+    commentCard.classList.add('comment');
+
+    const authorCard = document.createElement('h3');
+    const authorname = getUsers(comments.user_id)  
+    authorname.then(data => {
+      const [name] = data.map(user => user.name);
+      authorCard.textContent = name;
+      console.log(name);
+    });
+
+    const commentTitle = document.createElement('h4');
+    commentTitle.textContent = comments.title;
+
+    const commentText = document.createElement('p');
+    commentText.classList.add('card-text');
+    commentText.textContent = comments.comment;
+
+    const commentRating = document.createElement('div');
+    commentRating.classList.add('ratingMedia1');
+
+    const commentRatingStars = document.createElement('div');
+    commentRatingStars.classList.add('Stars');
+    commentRatingStars.classList.add('starsWidth');
+    commentRatingStars.setAttribute('id','mediaStars');
+    commentRatingStars.setAttribute('style', `--rating: ${comments.rating};`);
+
+    commentCard.appendChild(authorCard);
+    commentCard.appendChild(commentTitle);
+    commentCard.appendChild(commentText);
+    commentCard.appendChild(commentRating);
+    commentRating.appendChild(commentRatingStars);
+    
+    return commentCard;
+  }
+
+  comentcard.innerHTML = ''
+
+  comments.forEach(comments => {
+    const newComment = commentsList(comments)
+    comentcard.appendChild(newComment);
+  });
+
+}
+
+
+renderComments();
